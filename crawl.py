79998df4f92f51c12067cf1f145b619471367583
wxn <pkuwxn@foxmail.com>
2016-04-23 12:04:43 +0800

    Make crawl.py's input compatible with extract_problems.py's output.

--- crawl.py
+++ crawl.py
@@ -4,11 +4,11 @@ common.prepare(use_proxy=True)
 
 
 for problem in open("problems.txt"):
-    problem = problem.strip()
+    pid, ctitle, problem, _ = problem.strip().split('\t')
     url = "https://leetcode.com/problems/%s/" % problem
     html = common.simple_read(url)
 
-    with open("raw/%s.html" % problem, "w") as outf:
+    with open("raw/%d.%s.html" % problem, "w") as outf:
         outf.write(html)
 
     print "Done crawling", url
